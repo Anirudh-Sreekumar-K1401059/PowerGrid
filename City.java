68dde9f46f6084c031772b0a79a7848785f3bb2b
VyasSiyaK2006625 <k2006625@students.katyisd.org>
2026-03-30 09:14:22 -0500

    Create City.java

---
+++ City.java
@@ -0,0 +1,18 @@
+public class City {
+    String name;
+    String region;
+    int spotsTaken;
+    int cost;
+    boolean isAvailable;
+    boolean isValidLoc;
+    ArrayList<Player> occupants;
+    public void addOccupant(Player player){
+        
+    }
+    public void changeCost(int newCost){
+        cost = newCost;
+    }
+    public boolean isOpen(){
+       
+    }
+}
